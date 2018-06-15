63d9a33d0fb389756be716076282d7da5226dc38
antox89 <ator89@unitec.edu>
2018-06-15 13:58:29 -0600

    GUI Piezas

--- Lab08_AngelT/src/lab08_angelt/AUDI.java
+++ Lab08_AngelT/src/lab08_angelt/AUDI.java
@@ -1,6 +1,8 @@
 
 package lab08_angelt;
 
+import javax.swing.DefaultListModel;
+
 /**
  *
  * @author angel
@@ -18,46 +20,172 @@ public class AUDI extends javax.swing.JFrame {
     private void initComponents() {
 
         jd_carros = new javax.swing.JDialog();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jl_piezas = new javax.swing.JList<>();
+        jLabel1 = new javax.swing.JLabel();
         jd_piezas = new javax.swing.JDialog();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        tf_nombrePieza = new javax.swing.JTextField();
+        tf_materialPieza = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        tf_tiempo = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        bt_crearPieza = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
+        jm_sistema = new javax.swing.JMenu();
+        jmi_carros = new javax.swing.JMenuItem();
+        jmi_piezas = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
+
+        jl_piezas.setModel(new DefaultListModel());
+        jScrollPane1.setViewportView(jl_piezas);
+
+        jLabel1.setText("Lista de Piezas");
 
         javax.swing.GroupLayout jd_carrosLayout = new javax.swing.GroupLayout(jd_carros.getContentPane());
         jd_carros.getContentPane().setLayout(jd_carrosLayout);
         jd_carrosLayout.setHorizontalGroup(
             jd_carrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(jd_carrosLayout.createSequentialGroup()
+                .addContainerGap(285, Short.MAX_VALUE)
+                .addGroup(jd_carrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_carrosLayout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(204, 204, 204))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_carrosLayout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(228, 228, 228))))
         );
         jd_carrosLayout.setVerticalGroup(
             jd_carrosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_carrosLayout.createSequentialGroup()
+                .addContainerGap(78, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 252, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(105, 105, 105))
         );
+
+        jLabel2.setText("Nombre:");
+
+        jLabel3.setText("Material:");
+
+        jLabel4.setText("Tiempo de elaboración:");
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Nombre", "Material", "Tiempo Elaboración"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.Integer.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(jTable1);
+
+        jLabel5.setText("Mins");
+
+        bt_crearPieza.setText("Crear Pieza");
+
+        jLabel6.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(51, 153, 255));
+        jLabel6.setText("Creación de Piezas");
 
         javax.swing.GroupLayout jd_piezasLayout = new javax.swing.GroupLayout(jd_piezas.getContentPane());
         jd_piezas.getContentPane().setLayout(jd_piezasLayout);
         jd_piezasLayout.setHorizontalGroup(
             jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_piezasLayout.createSequentialGroup()
+                .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jd_piezasLayout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jd_piezasLayout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(tf_materialPieza))
+                            .addGroup(jd_piezasLayout.createSequentialGroup()
+                                .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(jd_piezasLayout.createSequentialGroup()
+                                        .addComponent(jLabel2)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(tf_nombrePieza, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(jd_piezasLayout.createSequentialGroup()
+                                        .addComponent(jLabel4)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(tf_tiempo, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jLabel5)))
+                                .addGap(0, 0, Short.MAX_VALUE))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jd_piezasLayout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(bt_crearPieza)))
+                .addGap(18, 18, 18)
+                .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel6)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 396, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(39, 39, 39))
         );
         jd_piezasLayout.setVerticalGroup(
             jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(jd_piezasLayout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addComponent(jLabel6)
+                .addGap(18, 18, 18)
+                .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 206, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jd_piezasLayout.createSequentialGroup()
+                        .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(tf_nombrePieza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(tf_materialPieza, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jd_piezasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(tf_tiempo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel5))
+                        .addGap(18, 18, 18)
+                        .addComponent(bt_crearPieza)))
+                .addContainerGap(33, Short.MAX_VALUE))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jMenu1.setText("Sistema");
+        jm_sistema.setText("Sistema");
 
-        jMenuItem1.setText("Carros");
-        jMenu1.add(jMenuItem1);
+        jmi_carros.setText("Carros");
+        jmi_carros.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_carrosActionPerformed(evt);
+            }
+        });
+        jm_sistema.add(jmi_carros);
 
-        jMenuItem2.setText("Piezas");
-        jMenu1.add(jMenuItem2);
+        jmi_piezas.setText("Piezas");
+        jm_sistema.add(jmi_piezas);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(jm_sistema);
 
         jMenu2.setText("Edit");
         jMenuBar1.add(jMenu2);
@@ -77,6 +205,13 @@ public class AUDI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jmi_carrosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_carrosActionPerformed
+        jd_carros.pack();
+        jd_carros.setModal(true);
+        jd_carros.setLocationRelativeTo(this);
+        jd_carros.setVisible(true);
+    }//GEN-LAST:event_jmi_carrosActionPerformed
 
     /**
      * @param args the command line arguments
@@ -114,12 +249,26 @@ public class AUDI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
+    private javax.swing.JButton bt_crearPieza;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     private javax.swing.JDialog jd_carros;
     private javax.swing.JDialog jd_piezas;
+    private javax.swing.JList<String> jl_piezas;
+    private javax.swing.JMenu jm_sistema;
+    private javax.swing.JMenuItem jmi_carros;
+    private javax.swing.JMenuItem jmi_piezas;
+    private javax.swing.JTextField tf_materialPieza;
+    private javax.swing.JTextField tf_nombrePieza;
+    private javax.swing.JTextField tf_tiempo;
     // End of variables declaration//GEN-END:variables
 }
